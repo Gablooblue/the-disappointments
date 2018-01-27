6280c2c77c6966e87f3537bb847af62fe37199f3
gablooblue <gabrielhenry.lopez@gmail.com>
2018-01-28 02:04:09 +0800

    imma nap

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
     resources :cities do 
 	resources :delicacies
     end
+
+    resources :delicacies
     resources :regions
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root "splash#index"
